03e09abfa64f0224934b1196fee7b5f7132e07e8
JagWasTaken <jagroopg@hotmail.com>
2024-11-14 21:49:15 -0700

    Revise and cleanup answers

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convert = fahrenheit - 32
+  let answer = convert * 0.5555555
+  return Math.round(answer * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let product = celsius * 1.8
+  let sum = product + 32
+  return Math.round(sum * 10) / 10
 };
 
 // Do not edit below this line
